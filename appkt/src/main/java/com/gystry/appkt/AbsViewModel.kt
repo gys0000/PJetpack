4820825b04709551d96b6d4554cc27c3d79fefa5
gystry <gustry@163.com>
2021-04-01 18:44:41 +0800

    feat:pageList的应用

---
+++ appkt/src/main/java/com/gystry/appkt/AbsViewModel.kt
@@ -0,0 +1,68 @@
+package com.gystry.appkt
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.paging.DataSource
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+
+/**
+ * @author gystry
+ * 创建日期：2021/4/1 17
+ * 邮箱：gystry@163.com
+ * 描述：
+ */
+abstract class AbsViewModel<T> : ViewModel() {
+    var createDataSource: DataSource<Any, T>? = null
+    val boundaryPageData
+        get() = MutableLiveData<Boolean>()
+    private val config: PagedList.Config by lazy {
+        PagedList.Config.Builder()
+                .setPageSize(10)
+                .setInitialLoadSizeHint(12)
+                .build()
+    }
+
+    //监听pagelist数据加载的状态
+    private val boundaryCallback: PagedList.BoundaryCallback<T> by lazy {
+        object : PagedList.BoundaryCallback<T>() {
+            override fun onZeroItemsLoaded() {
+                //从PagedList的数据源的初始加载返回零项时被调用
+                boundaryPageData.postValue(false)
+            }
+
+            override fun onItemAtFrontLoaded(itemAtFront: T) {
+                //当列表的第一条数据被加载的时候会被调用
+                boundaryPageData.postValue(true)
+            }
+
+            override fun onItemAtEndLoaded(itemAtEnd: T) {
+                //当pageList的最后一条数据被加载到屏幕上的时候，这个方法会被调用
+                super.onItemAtEndLoaded(itemAtEnd)
+            }
+        }
+    }
+
+    private val factory: DataSource.Factory<Any, T> by lazy {
+        object : DataSource.Factory<Any, T>() {
+            override fun create(): DataSource<Any, T> {
+                createDataSource = createDataSource()
+                return createDataSource as DataSource<Any, T>
+            }
+        }
+    }
+
+    fun getDataSource():DataSource<Any, T>?{
+        return createDataSource
+    }
+
+    val pageData: LiveData<PagedList<T>>
+        get() = LivePagedListBuilder(factory, config)
+                .setInitialLoadKey(0)//加载初始化数据的时候需要传递的数据
+                .setBoundaryCallback(boundaryCallback)//
+                .build()
+
+
+    abstract fun createDataSource(): DataSource<Any, T>
+}
--- appkt/src/main/java/com/gystry/appkt/ui/home/HomeViewModel.kt
+++ appkt/src/main/java/com/gystry/appkt/ui/home/HomeViewModel.kt
@@ -3,11 +3,37 @@ package com.gystry.appkt.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.paging.DataSource
+import androidx.paging.ItemKeyedDataSource
+import com.gystry.appkt.AbsViewModel
+import com.gystry.appkt.model.Feed
+import java.util.*
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel : AbsViewModel<Feed>() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    override fun createDataSource(): DataSource<Any, Feed> {
+        TODO("Not yet implemented")
     }
-    val text: LiveData<String> = _text
+
+    val mDataSource= object :ItemKeyedDataSource<Int,Feed>(){
+
+        override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<Feed>) {
+            //加载初始化数据的
+        }
+
+        override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Feed>) {
+            //加载分页数据
+        }
+
+        override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Feed>) {
+            //能够向前加载  比如进入页面的时候加载的是第三页，那么向上翻动的时候可以加载第二页第一页
+            //一般用不到，返回一个空的列表
+            callback.onResult(Collections.emptyList())
+        }
+
+        override fun getKey(item: Feed): Int {
+            TODO("Not yet implemented")
+        }
+
+    }
 }
