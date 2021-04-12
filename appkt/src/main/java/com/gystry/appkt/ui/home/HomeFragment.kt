d89a700295436fa7c01dc3bad2b4d667b45826ca
gystry <gustry@163.com>
2021-04-12 16:46:15 +0800

    feat:master第三次提交

--- appkt/src/main/java/com/gystry/appkt/ui/home/HomeFragment.kt
+++ appkt/src/main/java/com/gystry/appkt/ui/home/HomeFragment.kt
@@ -9,12 +9,10 @@ import com.scwang.smartrefresh.layout.api.RefreshLayout
 @FragmentDestination(pageUrl = "main/tabs/home", asStarter = true)
 class HomeFragment : AbsListFragment<Feed, HomeViewModel, FeedAdapter.ViewHolder>() {
 
-    //这是在twoWork上的第一次提交 时间是16：16
     override fun getAdapter(): PagedListAdapter<Feed, FeedAdapter.ViewHolder> {
         val feedType = if (arguments == null) "all" else requireArguments().getString("fedType")
         return FeedAdapter(context, feedType!!)
     }
-    //这是在twoWork上的第二次提交 时间是16：19
     override fun onLoadMore(refreshLayout: RefreshLayout) {
         super.onLoadMore(refreshLayout)
     }
@@ -22,11 +20,9 @@ class HomeFragment : AbsListFragment<Feed, HomeViewModel, FeedAdapter.ViewHolder
     override fun onRefresh(refreshLayout: RefreshLayout) {
         super.onRefresh(refreshLayout)
     }
-    //这是master上的第二次提交   时间大约在16：20
 
     override fun afterCreateView() {
     }
 
-    //这是master上的第一次提交   时间大约在16：18
 
 }
